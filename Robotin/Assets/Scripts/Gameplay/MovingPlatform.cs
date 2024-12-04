da542d8f91edbebd96e51c4f9399456ee28a9d55
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2024-12-04 20:07:34 +0100

    moving platform bug fixed

--- Robotin/Assets/Scripts/Gameplay/MovingPlatform.cs
+++ Robotin/Assets/Scripts/Gameplay/MovingPlatform.cs
@@ -9,7 +9,8 @@ public class MovingPlatform : MonoBehaviour
     Vector3 startPosition;
     Vector3 endPosition = new Vector3(5, 0, 0);
     bool movingRight = true;
-
+    [SerializeField] private GameObject player;
+     
     void Start()
     {
         startPosition = transform.position;
@@ -17,6 +18,9 @@ public class MovingPlatform : MonoBehaviour
 
     void Update()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 movement = Vector3.zero;
+
         if (movingRight)
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
@@ -32,6 +36,28 @@ public class MovingPlatform : MonoBehaviour
             {
                 movingRight = true;
             }
+        }
+
+        transform.position += movement;
+
+        if (player != null)
+        {
+            player.transform.position += movement;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = null;
         }
     }
 }

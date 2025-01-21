b46ea7186a4deca01094ed73c47b0ad2d0385a0f
Marco Di Modica Elias <marcodimodica@protonmail.com>
2025-01-21 01:04:15 +0100

    playerAnimationUpdate(NotAll)

---
+++ Robotin/Assets/UpdateAnimationsRobotin.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAnimationsRobotin : MonoBehaviour
+{
+    public string walkAnimationName = "Walk";
+    public string upVelocityAnimationName = "Jumping";
+    public string downVelocityAnimationName = "Falling";
+    public string ChargingAnimationName = "ChargingGround";
+    public string ChargingWallAnimationName = "ChargingWall";
+    public string WallSlidingAnimationName = "WallSliding";
+
+    public Animator animator;
+
+    public RobotinMovement robotinMovement;
+    public RobotinJump robotinJump;
+    public RobotinCollision robotinCollision;
+    public Rigidbody2D rb;
+
+    public enum PlayerState
+    {
+        Walking,
+        Jumping,
+        Falling,
+        ChargingGround,
+        ChargingWall,
+        WallSliding
+    }
+
+    public PlayerState playerState;
+
+    private void Update()
+    {        
+        UpdateRobotinState();
+        PlayAnimation();
+    }
+
+    private void UpdateRobotinState()
+    {
+        if (robotinCollision.IsNearWall && !robotinCollision.IsGrounded && rb.velocity.y < 0)
+        {
+            playerState = PlayerState.WallSliding;
+        }
+        else if (robotinJump.isJumpButtonPressed && robotinCollision.IsNearWall)
+        {
+            playerState = PlayerState.ChargingWall;
+        }
+        else if (robotinJump.isJumpButtonPressed && robotinCollision.IsGrounded)
+        {
+            playerState = PlayerState.ChargingGround;
+        }
+        else if (rb.velocity.y < 0 && !robotinCollision.IsGrounded && !robotinCollision.IsNearWall)
+        {
+            playerState = PlayerState.Falling;
+        }
+        else if (rb.velocity.y > 0 && !robotinCollision.IsGrounded && !robotinCollision.IsNearWall)
+        {
+            playerState = PlayerState.Jumping;
+        }
+        else if (robotinCollision.IsGrounded)
+        {
+            playerState = PlayerState.Walking;
+        }
+        else
+        {
+            playerState = PlayerState.Walking;
+        }
+    }
+
+    private void PlayAnimation()
+    {
+        switch (playerState)
+        {
+            case PlayerState.Walking:
+                animator.Play(walkAnimationName);
+                break;
+            case PlayerState.Jumping:
+                animator.Play(upVelocityAnimationName);
+                break;
+            case PlayerState.Falling:
+                animator.Play(downVelocityAnimationName);
+                break;
+            case PlayerState.ChargingGround:
+                animator.Play(ChargingAnimationName);
+                break;
+            case PlayerState.ChargingWall:
+                animator.Play(ChargingWallAnimationName);
+                break;
+            case PlayerState.WallSliding:
+                animator.Play(WallSlidingAnimationName);
+                break;
+            default:
+                animator.Play(walkAnimationName);
+                break;
+        }
+    }
+}
